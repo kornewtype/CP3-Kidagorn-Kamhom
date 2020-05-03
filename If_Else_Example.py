bfb755642a4da4713a16ea47f0372acdac5d92fc
kornewtype <64564695+kornewtype@users.noreply.github.com>
2020-05-03 17:59:48 +0700

    Create If_Else_Example.py

---
+++ If_Else_Example.py
@@ -0,0 +1,18 @@
+a = int(input("A Score : "))
+
+if a>=80:
+    print("Your Grade : A")
+elif a>=75:
+    print("Your Grade : B+")
+elif a>=70:
+    print("Your Grade : B")
+elif a>=65:
+    print("Your Grade : C+")
+elif a>=60:
+    print("Your Grade : C")
+elif a>=55:
+    print("Your Grade : D+")
+elif a>=50:
+    print("Your Grade : D")
+else:
+    print("Your Grade F")
