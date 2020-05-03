f6986c234ef74255ddfa6d3459db4d6208e5f317
kornewtype <64564695+kornewtype@users.noreply.github.com>
2020-05-03 11:18:26 +0700

    Brach_Test_Adding

--- Exercise4_Kidagorn_K.py
+++ Exercise4_Kidagorn_K.py
@@ -9,3 +9,5 @@ print("Foundation English = ",English)
 print("General Business = ",Business)
 print("Introduction to Computer System = ",Com_sys)
 print("Computer Programming =",Com_pro)
+
+print("Brach_Test")
